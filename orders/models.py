1373090fd1f81adc9c18bddc2ac2fd080433cf39
Your NameStormmaster169 <stormmaster169@gmail.com>
2020-09-11 00:16:09 +0300

    fix for post_save

--- orders/models.py
+++ orders/models.py
@@ -12,7 +12,7 @@ class Status(models.Model):
     updated = models.DateTimeField(auto_now_add=False, auto_now=True)
 
     def __str__(self):
-        return 'Статус: %s' % self.name
+        return "Статус %s" % self.name
 
     class Meta:
         verbose_name = 'Статус заказа'
@@ -20,55 +20,55 @@ class Status(models.Model):
 
 
 class Order(models.Model):
-    user = models.ForeignKey(User, blank=True, null=True, default=None, on_delete=models.CASCADE)
-    total_price = models.DecimalField(max_digits=10, decimal_places=2,
-                                      default=0)  # total price for all products in order
+    user = models.ForeignKey(User, blank=True, null=True, default=None)
+    total_price = models.DecimalField(max_digits=10, decimal_places=2, default=0)#total price for all products in order
     customer_name = models.CharField(max_length=64, blank=True, null=True, default=None)
     customer_email = models.EmailField(blank=True, null=True, default=None)
     customer_phone = models.CharField(max_length=48, blank=True, null=True, default=None)
     customer_address = models.CharField(max_length=128, blank=True, null=True, default=None)
     comments = models.TextField(blank=True, null=True, default=None)
-    status = models.ForeignKey(Status, on_delete=models.CASCADE)
+    status = models.ForeignKey(Status)
     created = models.DateTimeField(auto_now_add=True, auto_now=False)
     updated = models.DateTimeField(auto_now_add=False, auto_now=True)
 
     def __str__(self):
-        return 'Заказ: %s %s' % (self.id, self.status.name)
+        return "Заказ %s %s" % (self.id, self.status.name)
 
     class Meta:
         verbose_name = 'Заказ'
         verbose_name_plural = 'Заказы'
 
     def save(self, *args, **kwargs):
+
         super(Order, self).save(*args, **kwargs)
 
 
 class ProductInOrder(models.Model):
-    order = models.ForeignKey(Order, on_delete=models.CASCADE, blank=True, null=True, default=None)
-    product = models.ForeignKey(Product, on_delete=models.CASCADE, blank=True, null=True, default=None)
+    order = models.ForeignKey(Order, blank=True, null=True, default=None)
+    product = models.ForeignKey(Product, blank=True, null=True, default=None)
     nmb = models.IntegerField(default=1)
     price_per_item = models.DecimalField(max_digits=10, decimal_places=2, default=0)
-    total_price = models.DecimalField(max_digits=10, decimal_places=2, default=0)  # price*nmb
+    total_price = models.DecimalField(max_digits=10, decimal_places=2, default=0)#price*nmb
     is_active = models.BooleanField(default=True)
     created = models.DateTimeField(auto_now_add=True, auto_now=False)
     updated = models.DateTimeField(auto_now_add=False, auto_now=True)
 
     def __str__(self):
-        return '%s' % self.product.name
+        return "%s" % self.product.name
 
     class Meta:
         verbose_name = 'Товар в заказе'
         verbose_name_plural = 'Товары в заказе'
 
+
     def save(self, *args, **kwargs):
         price_per_item = self.product.price
         self.price_per_item = price_per_item
-        print(self.nmb)
+        print (self.nmb)
 
-        self.total_price = int(self.nmb) * self.price_per_item
+        self.total_price = int(self.nmb) * price_per_item
 
         super(ProductInOrder, self).save(*args, **kwargs)
-
 
 
 @disable_for_loaddata
@@ -89,25 +89,26 @@ post_save.connect(product_in_order_post_save, sender=ProductInOrder)
 
 class ProductInBasket(models.Model):
     session_key = models.CharField(max_length=128, blank=True, null=True, default=None)
-    order = models.ForeignKey(Order, on_delete=models.CASCADE, blank=True, null=True, default=None)
-    product = models.ForeignKey(Product, on_delete=models.CASCADE, blank=True, null=True, default=None)
+    order = models.ForeignKey(Order, blank=True, null=True, default=None)
+    product = models.ForeignKey(Product, blank=True, null=True, default=None)
     nmb = models.IntegerField(default=1)
     price_per_item = models.DecimalField(max_digits=10, decimal_places=2, default=0)
-    total_price = models.DecimalField(max_digits=10, decimal_places=2, default=0)  # price*nmb
+    total_price = models.DecimalField(max_digits=10, decimal_places=2, default=0)#price*nmb
     is_active = models.BooleanField(default=True)
     created = models.DateTimeField(auto_now_add=True, auto_now=False)
     updated = models.DateTimeField(auto_now_add=False, auto_now=True)
 
     def __str__(self):
-        return '%s' % self.product.name
+        return "%s" % self.product.name
 
     class Meta:
         verbose_name = 'Товар в корзине'
         verbose_name_plural = 'Товары в корзине'
 
+
     def save(self, *args, **kwargs):
         price_per_item = self.product.price
         self.price_per_item = price_per_item
-        self.total_price = int(self.nmb) * self.price_per_item
+        self.total_price = int(self.nmb) * price_per_item
 
         super(ProductInBasket, self).save(*args, **kwargs)
